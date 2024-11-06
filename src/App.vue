597536b6c0038d7514fbcd518177a32e43ff55ef
Shahnoza <shahnozaabdualimova191@gmail.com>
2024-11-06 10:34:29 +0500

    vue template with tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
